10576a012a9ea38c64e320174f501bf7fdfb238b
bkhalf <bahaakhalf8@gmail.com>
2020-04-28 01:10:50 +0200

    Update BTree.java

--- src/eg/edu/alexu/csd/filestructure/btree/BTree.java
+++ src/eg/edu/alexu/csd/filestructure/btree/BTree.java
@@ -1,6 +1,6 @@
 package eg.edu.alexu.csd.filestructure.btree;
 
-import eg.edu.alexu.csd.filestructure.btree.BNode.Point;
+//import eg.edu.alexu.csd.filestructure.btree.BNode.point;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,33 +23,46 @@ public class BTree<K extends Comparable<K>, V> implements IBTree <K,V> {
 		return root;
 	}
 
-	public void nonfullInsert(BNode x, Comparable key) {
+	public void nonfullInsert(BNode x, Comparable key,Object value) {
 		int i=x.getNumOfKeys();
 		if(x.isLeaf())
 		{
 			List<K> keys=new ArrayList<K>();
 			keys=x.getKeys();
+			List<K> values=new ArrayList<K>();
+			values=x.getValues();
+			if(keys.size()<Mindegree*2-1){
 			keys.add(null);
+			values.add(null);}
 			while(i >= 1 && key.compareTo(keys.get(i-1))<0 )//here find spot to put key.
 			{
 				keys.set(i,keys.get(i-1));
+				values.set(i,values.get(i-1));
 				i--;
 			}
 
 			keys.set(i, (K) key);
-
-			if(x.getNumOfKeys()==0)
-			    keys.add(i, (K) key);
-			else
+			values.set(i, (K) value);
+			if(x.getNumOfKeys()==0) {
+				keys.add(i, (K) key);
+				values.add(i, (K) value);
+			}
+			else {
 				keys.set(i, (K) key);
-
+				values.set(i, (K) value);
+			}
 			x.setKeys(keys);
+			x.setValues(values);
 		}
 		else {
 			int j = 0;
 			List<K> keys = new ArrayList<K>();
-			if(x.getKeys()!=null)
+			List<K> values = new ArrayList<K>();
+
+			if(x.getKeys()!=null){
 			keys = x.getKeys();
+			values=x.getValues();
+			}
 			while (j < x.getNumOfKeys() && key.compareTo(keys.get(j)) > 0)//find spot to recurse
 			{                         //on correct child
 				j++;
@@ -58,16 +71,18 @@ public class BTree<K extends Comparable<K>, V> implements IBTree <K,V> {
 			if (x.getChildren() != null) {
 				List<BNode<K, V>> childx = new ArrayList<BNode<K, V>>();
 				childx = x.getChildren();
-				if (childx.get(j-1).getNumOfKeys() == Mindegree * 2 - 1) {
+				if (childx.get(j).getNumOfKeys() == Mindegree * 2 - 1) {
 					split(x, j, childx.get(j));//call split on node x's ith child
 					List<K> keysx = new ArrayList<K>();
 					keysx = x.getKeys();
+					List<K> valuessx = new ArrayList<K>();
+					valuessx = x.getValues();
 					if (key.compareTo(keysx.get(j)) > 0) {
 						j++;
 					}
 				}
-
-				nonfullInsert(childx.get(j-1), key);//recurse
+				childx = x.getChildren();
+				nonfullInsert(childx.get(j), key,value);//recurse
 			}
 		}
 
@@ -76,30 +91,57 @@ public class BTree<K extends Comparable<K>, V> implements IBTree <K,V> {
 	@Override
 	public void insert(Comparable key, Object value) {
 
-		BNode root= (BNode) getRoot();
-		if(root==null){
-			this.root =new BNode(Mindegree*2,null);
-			List<K> keys=new ArrayList<K>();
+		BNode root = (BNode) getRoot();
+		if (root == null) {
+			this.root = new BNode(Mindegree * 2, null);
+			List<K> keys = new ArrayList<K>();
+			List<K> values = new ArrayList<K>();
+
 			keys.add((K) key);
+			values.add((K) value);
 			this.root.setKeys(keys);
+			this.root.setValues(values);
 			return;
 		}
 
-		if(root.getNumOfKeys()==Mindegree*2-1){
-			BNode s=new BNode(Mindegree*2,null);
-			this.root=s;
-			s.setLeaf(false);
-			s.setNumOfKeys(0);
+		if (root.getNumOfKeys() == Mindegree * 2 - 1) {
+//			boolean ss = true;
+//			if (root.getChildren() != null) {
+//
+//				ss = splitroot(key);
+//			}
+//			if (ss) {
+				BNode s = new BNode(Mindegree * 2, null);
+				this.root = s;
+				s.setLeaf(false);
+				s.setNumOfKeys(0);
+				List<BNode<K, V>> childs = new ArrayList<BNode<K, V>>();
+				childs.add(root);
+				s.setChildren(childs);
+				split(s, 0, root);
+				nonfullInsert(s, key, value); //call insert method
+			} else
+				nonfullInsert(root, key, value);//if its not full just insert it
 
-			s.children[0]=root;
-			split(s,0,root);
-			nonfullInsert(s, key); //call insert method
-		}
-		else
-			nonfullInsert(root,key);//if its not full just insert it
-		}
+
+	}
 
 
+
+//	public boolean splitroot(Comparable key){
+//
+//		List<IBTreeNode<K, V>>child=root.getChildren();
+//		int k=0;
+//		List<K> keys=root.getKeys();
+//		while (k<root.getNumOfKeys() && key.compareTo(keys.get(k))>0) {
+//			k++;
+//		}
+//		if(child.get(k).getNumOfKeys()==Mindegree*2-1){
+//
+//
+//		}
+//			return false;
+//		}
 
 
 	public void split(BNode x, int i, BNode y)
@@ -109,12 +151,18 @@ public class BTree<K extends Comparable<K>, V> implements IBTree <K,V> {
 		z.setNumOfKeys(Mindegree-1);
 		List<K> keys=new ArrayList<K>();
 		keys=y.getKeys();
+		List<K> values=new ArrayList<K>();
+		values=y.getValues();
 		List<K> keysz=new ArrayList<K>();
+		List<K> valuesz=new ArrayList<K>();
+
 		for(int j = 0; j < Mindegree - 1; j++)
 		{
 			keysz.add(keys.get(j+Mindegree));
+			valuesz.add(values.get(j+Mindegree));
 		}
 			z.setKeys(keysz);
+			z.setValues(valuesz);
 
 		if(!y.isLeaf())//if not leaf we have to reassign child nodes.
 		{
@@ -130,45 +178,51 @@ public class BTree<K extends Comparable<K>, V> implements IBTree <K,V> {
 			z.setChildren(child);
 
 		}
-		y.setNumOfKeys(Mindegree);
 
+		List<K> keysx=new ArrayList<K>();
+		List<K> valuesx=new ArrayList<K>();
 
+		if(x.getKeys()!=null) {
+			keysx = x.getKeys();
+			valuesx=x.getValues();
+		}
+		if(keysx.size()<Mindegree*2-1){
+		keysx.add(null);
+		valuesx.add(null);}
+		for(int j = x.getNumOfKeys(); j> i; j--)
+		{
+			keysx.set(j,keysx.get(j-1));
+			valuesx.set(j,valuesx.get(j-1));
+		}
+		List<K> keysy=new ArrayList<K>();
+		List<K> valuesy=new ArrayList<K>();
+
+		keysy=y.getKeys();
+		valuesy=y.getValues();
+		keysx.set(i,keysy.get(Mindegree-1));
+		valuesx.set(i,valuesy.get(Mindegree-1));
+		x.setKeys(keysx);
+		x.setValues(valuesx);
+
+		for(int j = 0; j < Mindegree ; j++)
+		{
+			keysy.remove(Mindegree-1);
+			valuesy.remove(Mindegree-1);
+		}
+		y.setKeys(keysy);
+		y.setValues(valuesy);
 		List<BNode<K, V>>childx=new ArrayList<BNode<K, V>>();
 		childx=x.getChildren();
-		BNode zz=z;
-		BNode yy=y;
-		if (childx==null){
-				List<BNode<K, V>>childxx=new ArrayList<BNode<K, V>>();
-				childxx.add(zz);
-				childxx.add(yy);
-				x.setChildren(childxx);
-			}
-			else{
-		for(int j = x.getNumOfKeys() ; j> i ; j--)
+		if(childx.size()<Mindegree*2)
+		childx.add(null);
+		for(int j = x.getNumOfKeys()-1 ; j> i ; j--)
 		{
 			childx.set(j+1,childx.get(j));
 //			x.child[j+1] = x.child[j]; //shift children of x
 		}
 		childx.set(i+1,z);
-		x.setChildren(childx);}
-		List<K> keysx=new ArrayList<K>();
-			if(x.getKeys()!=null)
-				keysx=x.getKeys();
-		keysx.add(null);
-		for(int j = x.getNumOfKeys(); j> i; j--)
-		{
-			keysx.set(j+1,keysx.get(j));
-		}
-		List<K> keysy=new ArrayList<K>();
-		keysy=y.getKeys();
-		keysx.set(i,keysy.get(Mindegree-1));
-		keysy.remove(Mindegree-1);
-        x.setKeys(keysx);
-//		for(int j = 0; j < Mindegree - 1; j++)
-//		{
-//			keysy.remove(Mindegree);
-//		}
-		y.setKeys(keysy);
+		x.setChildren(childx);
+
 
 	}
 
@@ -208,7 +262,6 @@ public class BTree<K extends Comparable<K>, V> implements IBTree <K,V> {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
 	public BNode searching(Comparable key) {
 		if(key==null)return null;
 		BNode node=root;
@@ -237,12 +290,10 @@ public class BTree<K extends Comparable<K>, V> implements IBTree <K,V> {
 		return null;
 		
 	}
-
 	public void print(IBTreeNode<K,V>  n)
 	{
 		List<K> keysn=new ArrayList<K>();
 		keysn=n.getKeys();
-
 		for(int i = 0; i < n.getNumOfKeys(); i++)
 		{
 			System.out.print(keysn.get(i)+" " );//this part prints root node
@@ -250,7 +301,7 @@ public class BTree<K extends Comparable<K>, V> implements IBTree <K,V> {
 
 		if(!n.isLeaf())//this is called when root is not leaf;
 		{
-			for(int j = 0; j < n.getNumOfKeys()  ; j++)//in this loop we recurse
+			for(int j = 0; j <= n.getNumOfKeys()  ; j++)//in this loop we recurse
 			{				  //to print out tree in
 				List<IBTreeNode<K, V>>childn=new ArrayList<IBTreeNode<K, V>>();
 				childn=n.getChildren();
