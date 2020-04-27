2f40324f99e4a4a54199fcd58a46d02ac5fe6aab
bkhalf <bahaakhalf8@gmail.com>
2020-04-28 01:12:20 +0200

    Update SearchEngine.java

--- src/eg/edu/alexu/csd/filestructure/btree/SearchEngine.java
+++ src/eg/edu/alexu/csd/filestructure/btree/SearchEngine.java
@@ -1,12 +1,32 @@
 package eg.edu.alexu.csd.filestructure.btree;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class SearchEngine implements ISearchEngine {
 
 	@Override
 	public void indexWebPage(String filePath) {
-		// TODO Auto-generated method stub
+		File file = new File(filePath); 
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance(); 
+		DocumentBuilder db = null;
+		try {
+			db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file);
+			doc.getDocumentElement().normalize();
+		} catch (SAXException | IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e1) {
+			e1.printStackTrace();
+		}  
 
 	}
 
