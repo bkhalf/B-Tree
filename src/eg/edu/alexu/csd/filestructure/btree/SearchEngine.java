735254c0bf8ea6f85cb221bc75e3197a52d3f9b0
bkhalf <bahaakhalf8@gmail.com>
2020-04-30 02:25:11 +0200

    Update SearchEngine.java

--- src/eg/edu/alexu/csd/filestructure/btree/SearchEngine.java
+++ src/eg/edu/alexu/csd/filestructure/btree/SearchEngine.java
@@ -1,6 +1,9 @@
 package eg.edu.alexu.csd.filestructure.btree;
 
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.management.RuntimeErrorException;
@@ -9,11 +12,12 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchEngine implements ISearchEngine {
 	/*How to define minDegree ??*/
-	IBTree<String , String> tree;
+	BTree<String , String> tree;
 	public SearchEngine(int minDegree){
 		if(minDegree <2)
 			throw new RuntimeErrorException(new ExceptionInInitializerError());
@@ -24,10 +28,10 @@ public class SearchEngine implements ISearchEngine {
 		/*if a null or a file with an empty name is entered*/
 		if(filePath == null || filePath.equals(""))
 			throw new RuntimeErrorException(new ExceptionInInitializerError());
-			File file = new File(filePath);
+		File file = new File(filePath);
 		/*if the file does not exist*/
 		if(!file.exists())
-				throw new RuntimeErrorException(new ExceptionInInitializerError());
+			throw new RuntimeErrorException(new ExceptionInInitializerError());
 		try {
 			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
@@ -57,12 +61,13 @@ public class SearchEngine implements ISearchEngine {
 		/*It gives a list of all files and directories in the directory.*/
 		String[] fileList = directory.list();
 		if(fileList!= null) {
+			int i=0;
 			for (String s : fileList) {
 				File f = new File(s);
 				if (f.isDirectory()) {
 					indexDirectory(s);
-				} else if (f.isFile()) {
-					indexWebPage(s);
+				} else  {
+					indexWebPage(directoryPath+"\\"+s);
 				}
 			}
 		}
@@ -70,17 +75,50 @@ public class SearchEngine implements ISearchEngine {
 
 	@Override
 	public void deleteWebPage(String filePath) {
-
+		/*if a null or a file with an empty name is entered*/
+		if(filePath == null || filePath.equals(""))
+			throw new RuntimeErrorException(new ExceptionInInitializerError());
+		File file = new File(filePath);
+		/*if the file does not exist*/
+		if(!file.exists())
+			throw new RuntimeErrorException(new ExceptionInInitializerError());
+		try {
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			Document document = documentBuilder.parse(file);
+			NodeList list = document.getElementsByTagName("doc");
+			for(int i = 0 ; i < list.getLength() ; i++){
+				Node node = list.item(i);
+				if(node.getNodeType() == Node.ELEMENT_NODE){
+					Element element = (Element) node;
+					tree.delete(element.getAttribute("id"));
+				}
+			}
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public List<ISearchResult> searchByWordWithRanking(String word) {
-		return null;
+		if(word == null )throw new RuntimeErrorException(null);
+
+		List<ISearchResult> re=new ArrayList<ISearchResult>();
+		if(word=="")return re;
+        tree.searchInValue(tree.getRoot(),true,word,re);
+        System.out.println(re.size()+"  SIZE ");
+		return re;
 	}
 
 	@Override
 	public List<ISearchResult> searchByMultipleWordWithRanking(String sentence) {
+		if(sentence == null )throw new RuntimeErrorException(null);
+
+		List<ISearchResult> re=new ArrayList<ISearchResult>();
+		if(sentence=="")return re;
 		return null;
 	}
+
+
 
 }
